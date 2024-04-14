66a54cd492e47796824fe5bdb1d04c25711bd66a
Baraa Jayousi <b.jayousi98@gmail.com>
2024-04-14 19:48:39 +0200

    initial login/register template

--- AuthenticationApp/views.py
+++ AuthenticationApp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import View
 
 # Create your views here.
+
+class Login(View):
+  template = 'login.html'
+  context = {}
+  def get(self, request):
+    return render(request=request, template_name= self.template, context=self.context)
