c43943fcd14e32c1bf83600067b7151616e945f2
Baraa Jayousi <b.jayousi98@gmail.com>
2024-04-15 06:31:27 +0200

    created forms module, added cripy application for customizing django fomrs

--- AuthenticationApp/views.py
+++ AuthenticationApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
+from .forms import *
 
 # Create your views here.
 
@@ -7,4 +8,7 @@ class Login(View):
   template = 'login.html'
   context = {}
   def get(self, request):
+    self.context = {
+      "loginForm" : LoginForm()
+    }
     return render(request=request, template_name= self.template, context=self.context)
